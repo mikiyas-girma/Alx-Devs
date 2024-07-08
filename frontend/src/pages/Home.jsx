cc66af39909ee7784ad28880023dc6be836caec2
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-07-08 12:24:04 +0300

    Info: fetched projects from the backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -7,12 +7,31 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Link } from "react-router-dom"
-import PathConstants from "@/routes/pathConstants"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router-dom";
+import PathConstants from "@/routes/pathConstants";
+import { useState, useEffect } from "react";
+import axiosInstance from "@/utils/axiosInstance";
+
+
 
 const Home = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        axiosInstance.get('/projects')
+            .then((res) => {
+                setProjects(res.data);
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+        , []);
+
+
     return (
         <>
             <Card className="min-h-screen">
@@ -29,7 +48,25 @@ const Home = () => {
                 </div>
                 <div className="mx-6 my-4 grid grid-cols-1 gap-4 sm:grid-cols-2 sm:m-2 sm:gap-2 md:gap-4 md:mx-6 md:my-4
                                 lg:grid-cols-3 flex flex-wrap">
-                    <div className="flex-1">
+                    {projects.map((project) => (
+                        <div key={project.id} className="flex-1">
+                            <Card className='h-full flex flex-col'>
+                                <CardHeader>
+                                    <CardTitle className='text-center'>{project.title}</CardTitle>
+                                    <CardDescription className='text-base tracking-wider
+                                                            text-current font-serif'>
+                                        {project.description}
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent className='mt-auto'>
+                                    <Button variant='outline' asChild>
+                                        <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', project.id)}`}>View</Link>
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    ))}
+                    {/* <div className="flex-1">
                         <Card className='h-full flex flex-col'>
                             <CardHeader>
                                 <CardTitle className='text-center'>CMMS </CardTitle>
@@ -46,159 +83,7 @@ const Home = () => {
                                 </Button>
                             </CardContent>
                         </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
-
-                    <div className="flex-1">
-                        <Card className='h-full flex flex-col'>
-                            <CardHeader>
-                                <CardTitle className='text-center'>CMMS </CardTitle>
-                                <CardDescription className='text-base tracking-wider
-                                                            text-current font-serif'>
-                                    The Construction and Machinery Material Management System
-                                    (CMMS) is a web-based application that helps in managing
-                                     ...
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className='mt-auto text-center'>
-                                <Button asChild>
-                                    <Link className="m-auto" to={`${PathConstants.PROJECT.replace(':id', '1')}`}>View</Link>
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div>
+                    </div> */}
 
 
                 </div>
