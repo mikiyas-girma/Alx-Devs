6470ad38e02b4562301a9b0a544b8a4eb8a25ec4
mikiyas-girma <mikiyas-girma@users.noreply.github.com>
2024-07-12 16:06:46 +0000

    deploy: 24705f3dab277722fe7cc14bc380d78768583037

--- assets/About-DKxX9iLN.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Ck9hF9J8.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx("div",{children:"About"})});export{e as default};
---
+++ assets/About-DOu68N-k.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BPfOWtap.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx("div",{children:"About"})});export{e as default};
