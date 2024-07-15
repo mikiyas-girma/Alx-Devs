4af2d7382fbc9430d1a95cc587a5da17fc7b7c9b
mikiyas-girma <mikiyas-girma@users.noreply.github.com>
2024-07-15 13:57:23 +0000

    deploy: b0bc0f19f26022b3904924ff45d444e82a14f814

---
+++ assets/About-DN6X4nlk.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CWn7KDYs.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx("div",{children:"About"})});export{e as default};
--- assets/CreateProject-Djo3Y4qU.js
+++ assets/CreateProject-Djo3Y4qU.js
@@ -1,4 +1,4 @@
-import{c as es,v as ze,r as p,w as Yi,x as Ki,y as ts,z as at,j as k,A as Xi,E as We,F as Re,G as ht,H as Wn,J as Ji,K as Qi,M as eo,S as to,N as ro,Q as no,U as ao,V as rs,W as so,Y as io,Z as oo,_ as lo,g as mt,$ as uo,a0 as pa,a1 as co,u as fo,b as po,I as ha,B as ho,a2 as mo}from"./index-a7uQSJ9g.js";import{C as vo,a as go,b as yo,c as bo,d as ma}from"./card-CSFmfl9h.js";import{T as xo}from"./textarea-Cud-Io56.js";/**
+import{c as es,v as ze,r as p,w as Yi,x as Ki,y as ts,z as at,j as k,A as Xi,E as We,F as Re,G as ht,H as Wn,J as Ji,K as Qi,M as eo,S as to,N as ro,Q as no,U as ao,V as rs,W as so,Y as io,Z as oo,_ as lo,g as mt,$ as uo,a0 as pa,a1 as co,u as fo,b as po,I as ha,B as ho,a2 as mo}from"./index-CWn7KDYs.js";import{C as vo,a as go,b as yo,c as bo,d as ma}from"./card-Ce3dUB3n.js";import{T as xo}from"./textarea-fE8hiQtz.js";/**
  * @license lucide-react v0.396.0 - ISC
  *
  * This source code is licensed under the ISC license.
